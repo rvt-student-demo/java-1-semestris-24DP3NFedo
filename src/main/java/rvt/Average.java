0032780a9b87b8e1649ea6d7300b7ab5481ebf94
24DP3NFedo <a242830nf@rvt.lv>
2025-09-18 14:09:23 +0300

    Uzdevums izpildits

---
+++ src/main/java/rvt/Average.java
@@ -0,0 +1,22 @@
+package rvt;
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Give the first number:");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Give the second number:");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Give the third number:");
+        int num3 = scanner.nextInt();
+
+        Double average = (num1 + num2 + num3) / 3.0;
+
+        System.out.println("The average is:" + average);
+
+    }
+}
