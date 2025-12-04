241ad9f91a14bf37b72e9cc9b6e774a0ec2d1fa4
24DP3NFedo <a242830nf@rvt.lv>
2025-12-04 12:06:59 +0200

    Exercise 2 - Two Arrays = done

--- src/main/java/rvt/Chapter_60.java
+++ src/main/java/rvt/Chapter_60.java
@@ -1,7 +1,7 @@
 package rvt;
 import java.util.Scanner;
 
-class Chapter_60
+class Exercize1
 {
   public static void main ( String[] args )
   {
@@ -15,5 +15,24 @@ class Chapter_60
  
     System.out.println( "Sum of all numbers = " + sum );
  
+   }
+}
+
+class Exercise2
+{
+  public static void main ( String[] args )
+  {
+    int[] val = {13, -4, 82, 17}; 
+    int[] twice = new int[val.length];
+    
+    System.out.println( "Original Array: " 
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+
+    for (int i = 0; i < val.length; i++){
+      twice[i] = val[i] + 2;
+    }
+ 
+    System.out.println( "New Array: " 
+        + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
    }
 }
