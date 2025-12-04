6bf8135ba3e98361f80184c9b27debe800e4ab30
24DP3NFedo <a242830nf@rvt.lv>
2025-12-04 12:11:14 +0200

    Excercise3 - Three Arrays = done

--- src/main/java/rvt/Chapter_60.java
+++ src/main/java/rvt/Chapter_60.java
@@ -36,3 +36,21 @@ class Exercise2
         + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
    }
 }
+
+class Exercise3
+{
+  public static void main ( String[] args )
+  {
+    int[] valA   = { 13, -22,  82,  17}; 
+    int[] valB   = {-12,  24, -79, -13};
+    int[] sum    = {  0,   0,   0,   0};
+    
+    sum[0] = valA[0] + valB[0];
+    sum[1] = valA[1] + valB[1];
+    sum[2] = valA[2] + valB[2];
+    sum[3] = valA[3] + valB[3];
+ 
+    System.out.println( "sum: " 
+        + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+   }
+}
