13480080a197abb99c43e8a4269dcb79d8c72ddb
24DP3NFedo <a242830nf@rvt.lv>
2025-12-04 12:00:25 +0200

    Excercise 1 - Array Sum = done

---
+++ src/main/java/rvt/Chapter_60.java
@@ -0,0 +1,19 @@
+package rvt;
+import java.util.Scanner;
+
+class Chapter_60
+{
+  public static void main ( String[] args )
+  {
+    int[] val = {0, 1, 2, 3}; 
+
+    int sum = 0;
+
+    for (int i = 0; i <val.length; i++){
+        sum += val[i];
+    }
+ 
+    System.out.println( "Sum of all numbers = " + sum );
+ 
+   }
+}
