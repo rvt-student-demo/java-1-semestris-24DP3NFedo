231d33d51e9b053874aee3dd90007abf0afad716
24DP3NFedo <a242830nf@rvt.lv>
2025-12-04 12:18:03 +0200

    Exercise5 - Reverse Order = done

--- src/main/java/rvt/Chapter_60.java
+++ src/main/java/rvt/Chapter_60.java
@@ -78,3 +78,27 @@ class Exercise4
         + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
    }
 }
+
+class Exercise5
+{
+  public static void main ( String[] args )
+  {
+    int[] val = {0, 1, 2, 3}; 
+    int temp;
+ 
+    System.out.println( "Original Array: " 
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+    
+      temp = val[0];
+      val[0] = val[3];
+      val[3] = temp;
+
+      temp = val[1];
+      val[1] = val[2];
+      val[2] = temp;
+
+ 
+    System.out.println( "Reversed Array: " 
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+   }
+}
