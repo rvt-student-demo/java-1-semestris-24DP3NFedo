8bec7eacc596c903d53c3d024b11b362df2e1b23
24DP3NFedo <a242830nf@rvt.lv>
2025-12-08 09:21:59 +0200

    Only These Numbers uzdevums - done

---
+++ src/main/java/rvt/OnlyTheseNumbers.java
@@ -0,0 +1,29 @@
+package rvt;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class OnlyTheseNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        while (true) {
+            int num = Integer.parseInt(scanner.nextLine());
+            if (num == -1) {
+                break;
+            }
+            numbers.add(num);
+        }
+
+        System.out.print("From Where? ");
+        int start = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("To Where? ");
+        int end = Integer.parseInt(scanner.nextLine());
+
+        for (int i = start; i <= end; i++) {
+            System.out.println(numbers.get(i));
+        }
+    }
+}
